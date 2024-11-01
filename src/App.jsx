0bf902aa0edccf1be56f7bf430335637a55dfef4
asifsarkar01 <aassiiff1155@gmail.com>
2024-11-01 10:15:16 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-4xl font-extrabold'>Vite + React</h1>
+
+      <button class="btn btn-info">Info</button>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,11 @@
+
+
+const Root = () => {
+    return (
+        <div>
+            <h3>I am frome React router ROOT</h3>
+        </div>
+    );
+};
+
+export default Root;
